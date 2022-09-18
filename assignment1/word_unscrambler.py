3b87696231fe9671c07998fff9d46cdefd95a492
jak307 <113345839+jak307@users.noreply.github.com>
2022-09-18 06:00:47 -0400

    Create word_unscrambler.py

---
+++ assignment1/word_unscrambler.py
@@ -0,0 +1,58 @@
+#open the text file, read the text fie, and store the data in a variable
+words = open('words.txt', 'r')
+
+#create an array from the words in the file
+wordlist = words.readlines()
+
+#remove the newline character and convert each word to lowercase
+wordclean = [word.strip().lower() for word in wordlist]
+
+#remove duplicates
+wordunique = list(set(wordclean))
+
+#sort the word list in lexiographic order
+wordunique.sort()
+
+#this function sorts a string into lexiographic order
+def signature(word):
+    return ''.join(sorted(word))
+
+#takes in a string and searches through the wordlist to find anagrams
+def anagram(myword):
+    for word in wordunique:
+        if(signature(word) == signature(myword)):
+            print(word)
+
+#makes sure user input is neither blank nor null
+def validate_input(user_input):
+    if user_input != "" and user_input != None:
+        return True
+    else:
+        return False
+
+#takes user input
+user_input = input("Please enter a word or a phrase: ")
+
+if validate_input(user_input):
+    #sorts user input into lexiographic order
+    sortedLetters = sorted(user_input)
+    #finds every unique combination of elements from a list
+    from itertools import combinations
+    #counts letters in word
+    counter = 6
+    while (counter > 2):
+        print(counter, " letters:")
+        theList = []
+        #creates list of all possible combinations of a word's letters
+        for i in combinations(sortedLetters, counter):
+            s = ""
+            s = s.join(i)
+            theList.append(s)
+        #removes duplicates from list
+        uniqueList = list(set(theList))
+        #sorts the list in alphabetical order
+        uniqueList.sort()
+        #looks for anagrams in list
+        for i in uniqueList:
+            anagram(i)
+        counter = counter - 1
