8ebee261ce209be5119376c275d140e50492636c
jak307 <113345839+jak307@users.noreply.github.com>
2022-10-31 19:23:50 -0400

    Add files via upload

--- assignment3/emr.py
+++ assignment3/emr.py
@@ -58,10 +58,12 @@ class Doctor:
                                          user='jak307',
                                          password='InfSci1500_4131521')
 
-            query = 'UPDATE doctor SET doctor_fname = %s WHERE user_id = %s;'
+            cursor = connection.cursor(prepared=True)
+            query = 'UPDATE doctor SET doctor_fname = %s WHERE doctor_id = %s;'
 
+            data_tuple = (self.__fname, self.__doctor_id)
             cursor = connection.cursor()
-            cursor.executemany(query, (self.__fname, self.__doctor_id))
+            cursor.execute(query, data_tuple)
             connection.commit()
 
         except mysql.connector.Error as error:
@@ -80,15 +82,34 @@ class Doctor:
                                          user='jak307',
                                          password='InfSci1500_4131521')
 
-            query = 'UPDATE doctor SET doctor_lname = %s WHERE user_id = %s;'
+            cursor = connection.cursor(prepared=True)
+            query = 'UPDATE doctor SET doctor_lname = %s WHERE doctor_id = %s;'
 
+            data_tuple = (self.__lname, self.__doctor_id)
             cursor = connection.cursor()
-            cursor.executemany(query, (self.__lname, self.__doctor_id))
+            cursor.execute(query, data_tuple)
             connection.commit()
 
         except mysql.connector.Error as error:
             print("Failed to insert record into MySQL table {}".format(error))
 
+        finally:
+            if connection.is_connected():
+                cursor.close()
+                connection.close()
+
+    def delete(self):
+        try:
+            connection = mysql.connector.connect(host='67.205.163.33',
+                                         database='jak307',
+                                         user='jak307',
+                                         password='InfSci1500_4131521')
+            cursor = connection.cursor(prepared=True)
+            query = """Delete from doctor where doctor_id = %s"""
+            cursor.execute(query, (self.__doctor_id,))
+            connection.commit()
+        except mysql.connector.Error as error:
+            print("parameterized query failed {}".format(error))
         finally:
             if connection.is_connected():
                 cursor.close()
@@ -154,10 +175,12 @@ class Patient:
                                          user='jak307',
                                          password='InfSci1500_4131521')
 
+            cursor = connection.cursor(prepared=True)
             query = 'UPDATE patient SET patient_fname = %s WHERE patient_id = %s;'
 
+            data_tuple = (self.__fname, self.__patient_id)
             cursor = connection.cursor()
-            cursor.executemany(query, (self.__fname, self.__patient_id))
+            cursor.execute(query, data_tuple)
             connection.commit()
 
         except mysql.connector.Error as error:
@@ -176,15 +199,34 @@ class Patient:
                                          user='jak307',
                                          password='InfSci1500_4131521')
 
+            cursor = connection.cursor(prepared=True)
             query = 'UPDATE patient SET patient_lname = %s WHERE patient_id = %s;'
 
+            data_tuple = (self.__lname, self.__patient_id)
             cursor = connection.cursor()
-            cursor.executemany(query, (self.__lname, self.__patient_id))
+            cursor.execute(query, data_tuple)
             connection.commit()
 
         except mysql.connector.Error as error:
             print("Failed to insert record into MySQL table {}".format(error))
 
+        finally:
+            if connection.is_connected():
+                cursor.close()
+                connection.close()
+
+    def delete(self):
+        try:
+            connection = mysql.connector.connect(host='67.205.163.33',
+                                         database='jak307',
+                                         user='jak307',
+                                         password='InfSci1500_4131521')
+            cursor = connection.cursor(prepared=True)
+            query = """Delete from patient where patient_id = %s"""
+            cursor.execute(query, (self.__patient_id,))
+            connection.commit()
+        except mysql.connector.Error as error:
+            print("parameterized query failed {}".format(error))
         finally:
             if connection.is_connected():
                 cursor.close()
@@ -237,7 +279,7 @@ class Procedure:
     def get_procedure_id(self):
         return self.__procedure_id
 
-    def set_procedure_name(self, procedure_name):
+    def set_name(self, procedure_name):
         self.__procedure = procedure_name
         try:
             connection = mysql.connector.connect(host='67.205.163.33',
@@ -245,15 +287,34 @@ class Procedure:
                                          user='jak307',
                                          password='InfSci1500_4131521')
 
+            cursor = connection.cursor(prepared=True)
             query = 'UPDATE procedures SET procedure_name = %s WHERE procedure_id = %s;'
 
+            data_tuple = (self.__procedure, self.__procedure_id)
             cursor = connection.cursor()
-            cursor.executemany(query, (self.__procedure, self.__procedure_id))
+            cursor.execute(query, data_tuple)
             connection.commit()
 
         except mysql.connector.Error as error:
             print("Failed to insert record into MySQL table {}".format(error))
 
+        finally:
+            if connection.is_connected():
+                cursor.close()
+                connection.close()
+
+    def delete(self):
+        try:
+            connection = mysql.connector.connect(host='67.205.163.33',
+                                         database='jak307',
+                                         user='jak307',
+                                         password='InfSci1500_4131521')
+            cursor = connection.cursor(prepared=True)
+            query = """Delete from procedures where procedure_id = %s"""
+            cursor.execute(query, (self.__procedure_id,))
+            connection.commit()
+        except mysql.connector.Error as error:
+            print("parameterized query failed {}".format(error))
         finally:
             if connection.is_connected():
                 cursor.close()
@@ -306,7 +367,7 @@ class Diagnosis:
     def get_diagnosis_id(self):
         return self.__diagnosis_id
 
-    def set_diagnosis_name(self, diagnosis_name):
+    def set_name(self, diagnosis_name):
         self.__diagnosis = diagnosis_name
         try:
             connection = mysql.connector.connect(host='67.205.163.33',
@@ -314,15 +375,34 @@ class Diagnosis:
                                          user='jak307',
                                          password='InfSci1500_4131521')
 
+            cursor = connection.cursor(prepared=True)
             query = 'UPDATE diagnosis SET diagnosis_name = %s WHERE diagnosis_id = %s;'
 
+            data_tuple = (self.__diagnosis, self.__diagnosis_id)
             cursor = connection.cursor()
-            cursor.executemany(query, (self.__diagnosis, self.__diagnosis_id))
+            cursor.execute(query, data_tuple)
             connection.commit()
 
         except mysql.connector.Error as error:
             print("Failed to insert record into MySQL table {}".format(error))
 
+        finally:
+            if connection.is_connected():
+                cursor.close()
+                connection.close()
+
+    def delete(self):
+        try:
+            connection = mysql.connector.connect(host='67.205.163.33',
+                                         database='jak307',
+                                         user='jak307',
+                                         password='InfSci1500_4131521')
+            cursor = connection.cursor(prepared=True)
+            query = """Delete from diagnosis where diagnosis_id = %s"""
+            cursor.execute(query, (self.__diagnosis_id,))
+            connection.commit()
+        except mysql.connector.Error as error:
+            print("parameterized query failed {}".format(error))
         finally:
             if connection.is_connected():
                 cursor.close()
@@ -386,10 +466,12 @@ class Visit:
                                          user='jak307',
                                          password='InfSci1500_4131521')
 
+            cursor = connection.cursor(prepared=True)
             query = 'UPDATE visit SET fk_doctor_id = %s WHERE visit_id = %s;'
 
+            data_tuple = (self.__doctor_id, self.__visit_id)
             cursor = connection.cursor()
-            cursor.executemany(query, (self.__doctor_id, self.__visit_id))
+            cursor.execute(query, data_tuple)
             connection.commit()
 
         except mysql.connector.Error as error:
@@ -408,10 +490,12 @@ class Visit:
                                          user='jak307',
                                          password='InfSci1500_4131521')
 
+            cursor = connection.cursor(prepared=True)
             query = 'UPDATE visit SET fk_patient_id = %s WHERE visit_id = %s;'
 
+            data_tuple = (self.__patient_id, self.__visit_id)
             cursor = connection.cursor()
-            cursor.executemany(query, (self.__patient_id, self.__visit_id))
+            cursor.execute(query, data_tuple)
             connection.commit()
 
         except mysql.connector.Error as error:
@@ -421,6 +505,24 @@ class Visit:
             if connection.is_connected():
                 cursor.close()
                 connection.close()
+
+    def delete(self):
+        try:
+            connection = mysql.connector.connect(host='67.205.163.33',
+                                         database='jak307',
+                                         user='jak307',
+                                         password='InfSci1500_4131521')
+            cursor = connection.cursor(prepared=True)
+            query = """Delete from visit where visit_id = %s"""
+            cursor.execute(query, (self.__visit_id,))
+            connection.commit()
+        except mysql.connector.Error as error:
+            print("parameterized query failed {}".format(error))
+        finally:
+            if connection.is_connected():
+                cursor.close()
+                connection.close()
+
     def to_json(self):
         fields_data = {
             "visit id" : self.__visit_id,
@@ -452,10 +554,12 @@ class Visit_Procedure:
                                          user='jak307',
                                          password='InfSci1500_4131521')
 
+            cursor = connection.cursor(prepared=True)
             query = 'UPDATE visit_procedure SET fk_visit_id = %s WHERE fk_procedure_id = %s;'
 
+            data_tuple = (self.__visit_id, self.__procedure_id)
             cursor = connection.cursor()
-            cursor.executemany(query, (self.__visit_id, self.__procedure_id))
+            cursor.execute(query, data_tuple)
             connection.commit()
 
         except mysql.connector.Error as error:
@@ -474,15 +578,35 @@ class Visit_Procedure:
                                          user='jak307',
                                          password='InfSci1500_4131521')
 
+            cursor = connection.cursor(prepared=True)
             query = 'UPDATE visit_procedure SET fk_procedure_id = %s WHERE fk_visit_id = %s;'
 
+            data_tuple = (self.__procedure_id, self.__visit_id)
             cursor = connection.cursor()
-            cursor.executemany(query, (self.__procedure_id, self.__visit_id))
+            cursor.execute(query, data_tuple)
             connection.commit()
 
         except mysql.connector.Error as error:
             print("Failed to insert record into MySQL table {}".format(error))
 
+        finally:
+            if connection.is_connected():
+                cursor.close()
+                connection.close()
+
+    def delete(self):
+        try:
+            connection = mysql.connector.connect(host='67.205.163.33',
+                                         database='jak307',
+                                         user='jak307',
+                                         password='InfSci1500_4131521')
+            cursor = connection.cursor(prepared=True)
+            query = """Delete from visit_procedure where fk_visit_id = %s 
+                        and fk_procedure_id = %s"""
+            cursor.execute(query, (self.__visit_id, self.__procedure_id))
+            connection.commit()
+        except mysql.connector.Error as error:
+            print("parameterized query failed {}".format(error))
         finally:
             if connection.is_connected():
                 cursor.close()
@@ -518,10 +642,12 @@ class Visit_Diagnosis:
                                          user='jak307',
                                          password='InfSci1500_4131521')
 
+            cursor = connection.cursor(prepared=True)
             query = 'UPDATE visit_diagnosis SET fk_visit_id = %s WHERE fk_diagnosis_id = %s;'
 
+            data_tuple = (self.__visit_id, self.__diagnosis_id)
             cursor = connection.cursor()
-            cursor.executemany(query, (self.__visit_id, self.__diagnosis_id))
+            cursor.execute(query, data_tuple)
             connection.commit()
 
         except mysql.connector.Error as error:
@@ -540,15 +666,35 @@ class Visit_Diagnosis:
                                          user='jak307',
                                          password='InfSci1500_4131521')
 
+            cursor = connection.cursor(prepared=True)
             query = 'UPDATE visit_diagnosis SET fk_diagnosis_id = %s WHERE fk_visit_id = %s;'
 
+            data_tuple = (self.__diagnosis_id, self.__visit_id)
             cursor = connection.cursor()
-            cursor.executemany(query, (self.__diagnosis_id, self.__visit_id))
+            cursor.execute(query, data_tuple)
             connection.commit()
 
         except mysql.connector.Error as error:
             print("Failed to insert record into MySQL table {}".format(error))
 
+        finally:
+            if connection.is_connected():
+                cursor.close()
+                connection.close()
+
+    def delete(self):
+        try:
+            connection = mysql.connector.connect(host='67.205.163.33',
+                                         database='jak307',
+                                         user='jak307',
+                                         password='InfSci1500_4131521')
+            cursor = connection.cursor(prepared=True)
+            query = """Delete from visit_diagnosis where fk_visit_id = %s 
+                        and fk_diagnosis_id = %s"""
+            cursor.execute(query, (self.__visit_id, self.__diagnosis_id))
+            connection.commit()
+        except mysql.connector.Error as error:
+            print("parameterized query failed {}".format(error))
         finally:
             if connection.is_connected():
                 cursor.close()
